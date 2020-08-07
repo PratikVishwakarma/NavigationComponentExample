0f007f3750cd49e0574fab2414d827274263ca7a
northout-pratik <Northout123>
2020-08-07 18:19:24 +0530

    init commit

---
+++ app/src/main/java/com/pratik/navigationcomponentexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pratik.navigationcomponentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
